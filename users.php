b91d37694c08dbc376295f7299c3fe46936dfcd2
arathbauer <arathbauer@student.tgm.ac.at>
2014-03-04 14:59:57 +0100

    Create users.php

---
+++ users.php
@@ -0,0 +1,35 @@
+<?php
+ 
+require("toro.php");
+ 
+if ( !file_exists("./user.json") ) {
+	$User = array();
+	$daten = array();
+	$daten["ID"] = "0";
+	$daten["name"] = "test";
+	$daten["password"] = "test";
+	$daten["mail"] = "test@tgm.ac.at";
+	$User[$daten["ID"]] = $daten;
+ 
+	file_put_contents("./user.json", json_encode($User));
+}
+
+class AccountHandler{
+
+	function put(){
+	$User=json_decode( file_get_contents("./user.json"));
+		if(isset($_GET["name"]) && isset($_GET["password"]) && isset($_GET["mail"])){
+			$User[] = $_POST;
+		}
+		file_put_contents("./user.json",json_encode($User));
+	}
+
+ToroHook::add("404", function() {
+	echo "Not found";
+});
+
+Toro::serve(array(
+	"/users.php" => "AccountHandler", //PUT - Registrieren
+	"/users/log/show.php" => "AccountHandler", //GET - Anzeigen
+));
+?>
